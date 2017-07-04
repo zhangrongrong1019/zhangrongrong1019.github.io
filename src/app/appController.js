ed364918359bce1742fc5372f32cb0d56b4b8b52
rongrong.zhang <rongrong.zhang@lincomb.com>
2017-07-04 17:53:35 +0800

    no message

--- src/app/appController.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-angular.module('app')
-	.controller('appController',[ '$rootScope', '$scope','ajaxRquest', '$state', function($rootScope,$scope,ajaxRquest,$state) {
-
-       $scope.goBack=function(){
-            history.go(-1);
-       }
-} ]);
